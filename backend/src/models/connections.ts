417c5434de9c109d3ecd8e5d684762dca5c7a323
quantxz <119700175+quantxz@users.noreply.github.com>
2023-04-11 09:52:04 -0300

    Add files via upload

--- backend/src/models/connections.ts
+++ backend/src/models/connections.ts
@@ -1,12 +1,15 @@
-import mysql from 'mysql';
+import knex from 'knex';
 import dotenv from 'dotenv';
-dotenv.config()
+dotenv.config();
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+const connection = knex({
+    client: 'mysql',
+    connection: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    }
 });
 
 export default connection;
